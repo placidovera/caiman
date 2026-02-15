9d4a37d48ced6c05e0c1b929f852d239b85d22e9
placidovera <buchetstefania@hotmail.com.ar>
2026-02-15 20:12:00 -0300

    con fotos

--- src/componetes/fotos.js
+++ src/componetes/fotos.js
@@ -1,7 +1,7 @@
 const fotosTapa =[
-    { id: 1,nombre:"negro" ,img:"/imagenes/negroUno.jpg"},
-    { id: 2,nombre:"beltran" ,img:"/imagenes/beltranUno.jpg"},
-    { id: 3,nombre:"panda" ,img:"/imagenes/pandaUno.jpg"}
+    { id: 1,nombre:"negro" ,img:"./imagenes/negroUno.jpg"},
+    { id: 2,nombre:"beltran" ,img:"./imagenes/beltranUno.jpg"},
+    { id: 3,nombre:"panda" ,img:"./imagenes/pandaUno.jpg"}
 
 ]
 
